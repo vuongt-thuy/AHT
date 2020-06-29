dcebd2cc5802e7418da6dd981bb7a9526d08e471
hieunguyen <hieunguyen5501552001@gmail.com>
2020-06-29 10:41:47 +0700

    change name theme

--- theme-frontend-basecamp/registration.php
+++ theme-frontend-basecamp/registration.php
@@ -6,4 +6,4 @@
 
 use \Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/AHT/theme-frontend-basecamp', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/AHT/basecamp', __DIR__);
